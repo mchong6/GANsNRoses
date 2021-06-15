f9dc0b627bea8dde8ff96405eac8bf843bcda9da
mchong6 <mchong6@illinois.edu>
2021-06-15 10:21:35 -0500

    remove cuda from fused act

--- op/fused_act.py
+++ op/fused_act.py
@@ -22,7 +22,6 @@ class FusedLeakyReLU(nn.Module):
 
 def fused_leaky_relu(input, bias, negative_slope=0.2, scale=2 ** 0.5):
     rest_dim = [1] * (input.ndim - bias.ndim - 1)
-    input = input.cuda()
     if input.ndim == 3:
         return (
             F.leaky_relu(
